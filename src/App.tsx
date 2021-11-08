b47ec85b8f15d55eee4d74c55c7823fc03b5d396
Gabriel <gabrielbittencourt57@gmail.com>
2021-11-08 16:22:05 -0300

    :truck: chore: Created route for the Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 
 export function App() {
   return (
-    <main>
-      <Home />
-    </main>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
