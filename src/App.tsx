d853e52632481cfc4e829b572ca77c8fe2b8740e
Gabriel <gabrielbittencourt57@gmail.com>
2021-11-08 02:05:07 -0300

    :truck: chore: Changed to the page Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { HeaderFilters } from './components/HeaderFilters'
-import searchImg from '/assets/icons/search.svg'
-import styles from './App.module.scss'
+import { Home } from './pages/Home'
 
 export function App() {
   return (
-    <main className={styles.container}>
-      <HeaderFilters />
-      <h1>Pokémon Bank</h1>
-      <p>Search for Pokémon by name or using the National Pokédex number.</p>
-      <label className={styles.searchContainer} htmlFor='search'>
-        <img src={searchImg} alt='Search icon' />
-        <input
-          className={styles.searchInput}
-          type='search'
-          name='search'
-          placeholder='What Pokémon are you looking for?'
-        />
-      </label>
+    <main>
+      <Home />
     </main>
   )
 }
