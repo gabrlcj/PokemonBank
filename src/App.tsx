3da4fe119f9a82b0835a93e25950994138fc976d
Gabriel <gabrielbittencourt57@gmail.com>
2021-11-09 01:04:36 -0300

    :truck: chore: Added the provider from context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { PokemonProvider } from './context/pokemon'
 import { Home } from './pages/Home'
 
 export function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
+    <PokemonProvider>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </PokemonProvider>
   )
 }
