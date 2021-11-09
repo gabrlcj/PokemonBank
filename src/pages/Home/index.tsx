57a5deca31183206eb97ae9e9e473627be2a55ea
Gabriel <gabrielbittencourt57@gmail.com>
2021-11-09 01:03:03 -0300

    :recycle: refactor: Made adjusts for the context api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,72 +1,13 @@
-import { useEffect, useState } from 'react'
-import { api } from '../../service/api'
 import { HeaderFilters } from '../../components/HeaderFilters'
 
 import searchImg from '/assets/icons/search.svg'
 import styles from './style.module.scss'
 import { PokeCards } from '../../components/PokeCards'
-
-type PokemonResults = {
-  name: string
-  url: string
-}
-
-type PokemonData = {
-  id: number
-  name: string
-  sprites: {
-    other: {
-      'official-artwork': {
-        front_default: string
-      }
-    }
-  }
-  types: {
-    0: {
-      type: {
-        name: string
-      }
-    }
-    1: {
-      type: {
-        name: string
-      }
-    }
-  }
-}
+import { useContext } from 'react'
+import { PokemonContext } from '../../context/pokemon'
 
 export function Home() {
-  const [pokemonData, setPokemonData] = useState<PokemonData[]>([])
-  const [loadMore, setLoadMore] = useState(
-    'https://pokeapi.co/api/v2/pokemon?limit=20'
-  )
-
-  const getAllPokemon = async () => {
-    const res = await api.get(loadMore)
-    setLoadMore(res.data.next)
-
-    function createPokemonObject(result: PokemonResults[]) {
-      result.forEach(async (pokemon) => {
-        const res = await api.get(`pokemon/${pokemon.name}`)
-        setPokemonData((prevState) => [...prevState, res.data])
-      })
-    }
-    createPokemonObject(res.data.results)
-  }
-
-  function order(pokemonData: PokemonData[]) {
-    pokemonData.sort(function (a, b) {
-      if (a.id > b.id) return 1
-      if (a.id < b.id) return -1
-      return 0
-    })
-  }
-
-  useEffect(() => {
-    getAllPokemon()
-  }, [])
-
-  order(pokemonData)
+  const { pokemonData, getAllPokemon } = useContext(PokemonContext)
 
   return (
     <>
@@ -85,9 +26,11 @@ export function Home() {
       {pokemonData.map((pokemon) => (
         <PokeCards key={Math.random()} pokemon={pokemon} />
       ))}
-      <button type='button' onClick={() => getAllPokemon()}>
-        Load More!
-      </button>
+      <div className={styles.loadMoreButton}>
+        <button type='button' onClick={() => getAllPokemon()}>
+          Load More!
+        </button>
+      </div>
     </>
   )
 }
