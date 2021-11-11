8c95af118c78df256428299c2c796f440742f6d2
Gabriel <gabrielbittencourt57@gmail.com>
2021-11-11 02:03:46 -0300

    :sparkles: chore: Created arrays for pokemon infos for the modal

---
+++ src/utils/pokemonGenerations.ts
@@ -0,0 +1,90 @@
+export const pokemonGenerations = [
+  {
+    generation: {
+      name: 'generation1',
+      title: 'Generation I',
+      starters: {
+        grass: '001',
+        fire: '004',
+        water: '007',
+      },
+    },
+  },
+  {
+    generation: {
+      name: 'generation2',
+      title: 'Generation II',
+      starters: {
+        grass: '152',
+        fire: '155',
+        water: '158',
+      },
+    },
+  },
+  {
+    generation: {
+      name: 'generation3',
+      title: 'Generation III',
+      starters: {
+        grass: '252',
+        fire: '255',
+        water: '258',
+      },
+    },
+  },
+  {
+    generation: {
+      name: 'generation4',
+      title: 'Generation IV',
+      starters: {
+        grass: '387',
+        fire: '390',
+        water: '393',
+      },
+    },
+  },
+  {
+    generation: {
+      name: 'generation5',
+      title: 'Generation V',
+      starters: {
+        grass: '495',
+        fire: '498',
+        water: '501',
+      },
+    },
+  },
+  {
+    generation: {
+      name: 'generation6',
+      title: 'Generation VI',
+      starters: {
+        grass: '650',
+        fire: '653',
+        water: '656',
+      },
+    },
+  },
+  {
+    generation: {
+      name: 'generation7',
+      title: 'Generation VII',
+      starters: {
+        grass: '722',
+        fire: '725',
+        water: '728',
+      },
+    },
+  },
+  {
+    generation: {
+      name: 'generation8',
+      title: 'Generation VII',
+      starters: {
+        grass: '810',
+        fire: '813',
+        water: '816',
+      },
+    },
+  },
+]
