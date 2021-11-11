a3f34be995d75e0cc8379ac42d2e4496af007ed7
Gabriel <gabrielbittencourt57@gmail.com>
2021-11-11 01:57:51 -0300

    :sparkles: feat: Imported function to open the modals

--- src/components/HeaderFilters/index.tsx
+++ src/components/HeaderFilters/index.tsx
@@ -1,23 +1,24 @@
+import { useContext } from 'react'
+import { PokemonContext } from '../../context/pokemon'
 import generationImg from '/assets/icons/generation.svg'
 import sortImg from '/assets/icons/sort.svg'
 import filterImg from '/assets/icons/filter.svg'
 
 import styles from './style.module.scss'
-import { useContext } from 'react'
-import { PokemonContext } from '../../context/pokemon'
 
 export default function HeaderFilters() {
-  const { handleSortOpen } = useContext(PokemonContext)
+  const { handleSortOpen, handleGenerationOpen, handleFilterOpen } =
+    useContext(PokemonContext)
   return (
     <header className={styles.headerIcons}>
       <div className={styles.filterButtons}>
-        <button type='button'>
+        <button type='button' onClick={handleGenerationOpen}>
           <img src={generationImg} alt='Generation icon' />
         </button>
         <button type='button' onClick={handleSortOpen}>
           <img src={sortImg} alt='Sort icon' />
         </button>
-        <button type='button'>
+        <button type='button' onClick={handleFilterOpen}>
           <img src={filterImg} alt='Filter icon' />
         </button>
       </div>
