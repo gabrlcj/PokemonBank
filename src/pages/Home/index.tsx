edea4b8f8cf4392eec581cad229dac9429c3a3ef
Gabriel <gabrielbittencourt57@gmail.com>
2021-11-12 21:44:59 -0300

    :recycle: refactor: Created a form for input and ul for the list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,26 +9,23 @@ import HeaderFilters from '../../components/HeaderFilters'
 import styles from './style.module.scss'
 
 export function Home() {
-  const { pokemonData, getAllPokemon } = useContext(PokemonContext)
+  const { getAllPokemon } = useContext(PokemonContext)
 
   return (
     <>
       <HeaderFilters />
       <h1>Pokémon Bank</h1>
       <p>Search for Pokémon by name or using the National Pokédex number.</p>
-      <label htmlFor='search'>
+      <form>
         <input
           className={styles.searchInput}
           type='search'
           name='search'
           placeholder='What Pokémon are you looking for?'
         />
-      </label>
-      <ul>
-        {pokemonData?.map((pokemon) => (
-          <PokeCards key={Math.random()} pokemon={pokemon} />
-        ))}
-      </ul>
+        <label htmlFor='search' />
+      </form>
+      <PokeCards />
       <div className={styles.loadMoreButton}>
         <button type='button' onClick={() => getAllPokemon()}>
           Load More!
