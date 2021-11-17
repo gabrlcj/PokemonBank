658d5754e2db222b24b5ab0516b796dee684de15
Gabriel <gabrielbittencourt57@gmail.com>
2021-11-17 17:43:55 -0300

    :sparkles: feat: Made the search input functional and added search button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
-import { useContext } from 'react'
+import { FormEvent, useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { PokemonContext } from '../../context/pokemon'
 import { PokeCards } from '../../components/PokeCards'
 import { ModalSort } from '../../components/ModalSort'
@@ -7,25 +8,40 @@ import { ModalGeneration } from '../../components/ModalGeneration'
 import HeaderFilters from '../../components/HeaderFilters'
 
 import styles from './style.module.scss'
+import { api } from '../../service/api'
 
 export function Home() {
-  const { getAllPokemon } = useContext(PokemonContext)
+  const { pokemonData, getAllPokemon } = useContext(PokemonContext)
+  const [search, setSearch] = useState('')
+  const navigate = useNavigate()
+
+  const onSubmitHandler = async (e: FormEvent) => {
+    e.preventDefault()
+    const res = await api.get(`pokemon/${search}`)
+    if (res.status === 404) {
+      navigate('/')
+    } else {
+      navigate(`pokemon/${search}`)
+    }
+  }
 
   return (
     <main>
       <HeaderFilters />
       <h1>Pokémon Bank</h1>
       <p>Search for Pokémon by name or using the National Pokédex number.</p>
-      <form>
+      <form className={styles.searchContainer} onSubmit={onSubmitHandler}>
         <input
           className={styles.searchInput}
-          type='search'
-          name='search'
+          type='text'
+          name='text'
           placeholder='What Pokémon are you looking for?'
+          onChange={(e) => setSearch(e.target.value)}
         />
-        <label htmlFor='search' />
+        <label htmlFor='text' />
+        <button type='submit'>Search</button>
       </form>
-      <PokeCards />
+      <PokeCards pokemonData={pokemonData} />
       <div className={styles.loadMoreButton}>
         <button type='button' onClick={() => getAllPokemon()}>
           Load More!
